4b4c654376848cd97e4c422210fb890c0c524901
Hugo CHAPPER <chapperhugo@gmail.com>
2022-04-13 10:04:41 +0200

    gros update

--- app/src/main/java/com/example/walnut/AfficheMatiereWebActivity.java
+++ app/src/main/java/com/example/walnut/AfficheMatiereWebActivity.java
@@ -22,7 +22,23 @@ public class AfficheMatiereWebActivity extends AppCompatActivity {
         if (messageRecu.equalsIgnoreCase ("BLOC3")){
             affichageWeb.loadUrl ("file:///android_asset/BLOC3.html");
 
-        } else
-            affichageWeb.loadUrl ("file:///android_asset/erreur.html");
+        }
+        else if(messageRecu.equalsIgnoreCase("BLOC2")) {
+            affichageWeb.loadUrl("file:///android_asset/BLOC2.html");
+        }
+        else if(messageRecu.equalsIgnoreCase ("BLOC1")){
+            affichageWeb.loadUrl("file:///android_asset/BLOC1.html");
+        }
+        else if(messageRecu.equalsIgnoreCase ("Espagnol")){
+            affichageWeb.loadUrl("file:///android_asset/Espagnol.html");
+        }
+        else if(messageRecu.equalsIgnoreCase ("Anglais")){
+            affichageWeb.loadUrl("file:///android_asset/Anglais.html");
+        }
+        else if(messageRecu.equalsIgnoreCase ("Maths")){
+            affichageWeb.loadUrl("file:///android_asset/Maths.html");
+        }
+            else affichageWeb.loadUrl ("file:///android_asset/erreur.html");
     }
+
 }
